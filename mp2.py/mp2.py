ed1e3f1f8e4c30b08ae6f7f5ceeb2ba79ddd1840
Sonia-P5 <sp102@kent.ac.uk>
2026-03-04 11:49:11 +0000

    These codes are asking for the user's first and last name. This information will later be useful and added to the tuple.

--- mp2.py/mp2.py
+++ mp2.py/mp2.py
@@ -1,6 +1,20 @@
 print("---WELCOME TO THE PORTAL---")
-print("Clubs availanle")
-print("Chess, Tennis, Pottery, Archery, Dance")
+print("Clubs available at the Smile Factory Facility")
+print("Chess, Tennis, Pottery, Archery, and Dance")
 print("1. Create an Account")
 print("2. Overview of each club")
 print("3. Select a club(s) (Maximum 2)")
+
+a=input("Select and Option: ")
+
+chess=()
+tennis=()
+pottery=()
+archery=()
+dance=()
+
+if a=="1":
+    print("OPTION SELECTED: Create an account")
+    b=input("Type in your first name: ").capitalize()
+    c=input("Type in your last name: ").capitalize()
+    print(b)
