b8b0ea22535720df75c78125fa4cbf3f73055008
Sonia-P5 <sp102@kent.ac.uk>
2026-03-04 11:55:59 +0000

    Beggining to use the re module.

--- mp2.py/mp2.py
+++ mp2.py/mp2.py
@@ -13,8 +13,12 @@ pottery=()
 archery=()
 dance=()
 
+import re 
 if a=="1":
     print("OPTION SELECTED: Create an account")
     b=input("Type in your first name: ").capitalize()
     c=input("Type in your last name: ").capitalize()
-    print(b)
+    print("You must create a username. The username must begin with a number, and end with .SFF. Special character's are not allowed. It must be miniimum 8 characters. ")
+    username=input("Enter username:  ")
+    if re.search(".", username):
+        print(f"Username accepted. Welcome {username} ")
