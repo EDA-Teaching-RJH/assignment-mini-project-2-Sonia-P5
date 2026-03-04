086d7d8a3e5079ec91341c8ff3f0996a4d406a1e
Sonia-P5 <sp102@kent.ac.uk>
2026-03-04 13:09:41 +0000

    Adding class to gather extra information of the user.

--- mp2.py/mp2.py
+++ mp2.py/mp2.py
@@ -1,29 +1,45 @@
-print("---WELCOME TO THE PORTAL---")
-print("Clubs available at the Smile Factory Facility")
-print("Chess, Tennis, Pottery, Archery, and Dance")
-print("1. Create an Account")
-print("2. Overview of each club")
-print("3. Select a club(s) (Maximum 2)")
-print("4. Pay subscription for club")
+def main():
+    print("---WELCOME TO THE PORTAL---")
+    print("Clubs available at the Smile Factory Facility")
+    print("Chess, Tennis, Pottery, Archery, and Dance")
+    print("1. Create an Account")
+    print("2. Overview of each club")
+    print("3. Select a club(s) (Maximum 2)")
+    print("4. Pay subscription for club")
 
-a=input("Select and Option: ")
+    a=input("Select and Option: ")
 
-chess=()
-tennis=()
-pottery=()
-archery=()
-dance=()
+    participants_log=[]
+    chess=["Jessica", "Ryan"]
+    tennis=["Kelly"]
+    pottery=["Bryan", "Shelly", "Linda"]
+    archery=[]
+    dance=["Kai", "Zayn", "Cole", "Lloyd"]
 
-import re 
-if a=="1":
-    print("OPTION SELECTED: Create an account")
-    b=input("Type in your first name: ").capitalize()
-    c=input("Type in your last name: ").capitalize()
-    print("You must create a username. The username must begin with a number, and end with .SFF. Special character's are not allowed. It must be mininimum 8 characters. ")
-    username=input("Enter username:  ")
-    if re.search(r".+\.+", username):
-        print(f"Username accepted. Welcome {username} ")
-    else:
-        print("Username not valid")
-        username=input("Enter username:  ")
-        
+    import re 
+    if a=="1":
+        print("OPTION SELECTED: Create an account")
+        print("You must create a username. The username must begin with a number and end with .SmileFF. It must be minimum 8 characters. ")
+        username=str(input("Enter username:  "))
+        while username=="":
+            print("This is a mandatory field")
+            username=str(input("Enter username:  "))
+        if re.search(r"^\d.+\.+SmileFF$", username) and len(username)>=8: 
+            print("Valid Username")
+        else:
+                print("Username not valid. Opting out of Option 1") 
+
+        class Participant:
+             def __init__(self, name, last_name, occupation, age):
+                  self.name=name
+                  
+        name=input("What is your first name: ").capitalize()
+        last_name=input("What is your laste name: ").capitzalize()
+        occupation=input("What is your occupation? ")
+
+if __name__=="__main__":
+    main()
+
+
+
+ 
