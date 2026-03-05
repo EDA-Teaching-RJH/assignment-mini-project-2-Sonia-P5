f5f6200c5a43eb61156efc2315fa2c7ee92a1cbb
Sonia-P5 <sp102@kent.ac.uk>
2026-03-05 20:16:59 +0000

    Using classes to gather information of the user

--- Smile Fun Factory/body_of_code.py
+++ Smile Fun Factory/body_of_code.py
@@ -26,4 +26,38 @@ def main():
                     self.age=age
 
             def collecting():
+                user=get_user()
+                print(f"Information Collected: {user.name} {user.last_name}, {user.nationality}, {user.age} years old")
+            
+            def get_user():
+                name=input("Name: ").capitalize()
+                while name=="":
+                    print("This is a mandatory field")
+                    name=input("Name: ").capitalize()
+
+                last_name=input("Last Name: ").capitalize()
+                while last_name=="":
+                    print("This is a mandatory field")
+                    last_name=input("What is your last name: ").capitalize
+
+                nationality=input("Nationality: ").capitalize()
+                while nationality=="":
+                    print("This is a mandatory field")
+                    nationality=input("What is your nationality: ")
+                
+                while True:
+                    try:
+                        age=int(input("What is your age: "))
+                        break
+                    except ValueError:
+                        print("Use numbers to type your age")
+                        continue
+                user=User(name, last_name, nationality, age)
+                return user
+            
+            if __name__=="__main__":
+                collecting()
+
+                
+                            
     
