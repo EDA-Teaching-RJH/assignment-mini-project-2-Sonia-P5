eb45c9857118f66cd975257036d3ef453cd02acd
Sonia-P5 <sp102@kent.ac.uk>
2026-03-05 15:49:11 +0000

    I changed the original format incorporating a class to store the users first and last names, nationality and age

--- mp2.py/mp2.py
+++ mp2.py/mp2.py
@@ -1,4 +1,9 @@
 def main():
+    usernames=[]
+    Hockey=[]
+    Tennis=[]
+    Basketball=[]
+    Football=[]
     def options():
         print("---WELCOME TO THE PORTAL---")
         print("Clubs available at the Smile Factory Facility")
@@ -13,43 +18,62 @@ def main():
         a=input("Select option: ")
 
         def create_new_user():
-           import re
-           if a=="1":
-              name=input("What is your first name: ").capitalize()
+           class User:
+              def __init__(self, name, last_name, nationality, age):
+                 self.name=name
+                 self.last_name=last_name
+                 self.nationality=nationality
+                 self.age=age
+
+           def collecting():
+              user=get_user()
+              print(f"Information Collected: {user.name} {user.last_name}, {user.nationality}, {user.age} years old")
+           
+           def get_user():
+              name=input("Name: ").capitalize()
               while name=="":
-                        print("This is a mandatory field")
-                        name=input("What is your first name: ").capitalize()
+                 print("This is a mandatory field")
+                 name=input("Name: ").capitalize()
 
-              last_name=input("What is your last name: ").capitalize()
+              last_name=input("Last Name: ").capitalize()
               while last_name=="":
-                        print("This is a mandatory field")
-                        last_name=input("What is your last name: ").capitalize()
-
-              occupation=input("What is your occupation: ")
-              while occupation=="":
-                        print("This is a mandatory field")
-                        occupation=input("What is your occupation: ")
-              try:
-                        age=int(input("What is your age: "))
-              except ValueError:
-                        print("Use numbers to type your age")
-                        age=int(input("What is your age: "))
-
-              print("You must create a username. The username must begin with a number and end with the .SmileFF domain. It must be minimum 8 characters. ")
-              while True:
-                username=str(input("Enter username: "))
-                if re.search(r"^\d.+\.+SmileFF$", username) and len(username)>=8:
-                    print("Valid Username")
-                    break
-                else:
-                    print("Username not valid.")
-                    continue
-                
-              print("Account Succesfully Created. Information stored")
-                 
+                 print("This is a mandatory field")
+                 last_name=input("What is your last name: ").capitalize()
               
+              nationality=input("Nationality: ").capitalize()
+              while nationality=="":
+                 print("This is a mandatory field")
+                 nationality=input("What is your nationality: ")
+              
+              while True:
+                 try:
+                    age=int(input("What is your age: "))
+                    break
+                 except ValueError:
+                    print("Use numbers to type your age")
+                    continue
+              user=User(name, last_name, nationality, age)
+              return user
+           
+           if __name__=="__main__":
+            collecting()
+                 
+            import re
+            print("You must create a username. The username must begin with a number and end with the .SmileFF domain. It must be minimum 8 characters. ")
+            while True:
+               username=str(input("Enter username: "))
+               if re.search(r"^\d.+\.+SmileFF$", username) and len(username)>=8:
+                  print("Valid Username")
+                  break
+               else:
+                  print("Username not valid.")
+                  continue
 
-        create_new_user()
+            usernames.append(username)
+            print("Account Succesfully Created. Information stored")
+
+        if __name__=="__main__":       
+         create_new_user()
 
     if __name__=="__main__":
      options()
--- mp2.py/test2.py
+++ mp2.py/test2.py
@@ -1,5 +1,38 @@
-try:
-    age=int(input("What is your age:  "))
-except ValueError:
-    print("Use numbers to type your age")
-    age=int(input("What is your age:  "))
+class User:
+    def __init__(self, name, last_name, nationality, age):
+        self.name=name
+        self.last_name=last_name
+        self.nationality=nationality
+        self.age=age
+    
+def collecting():
+    user=get_user()
+    print(f"Information Collected: {user.name} {user.last_name}, {user.nationality}, {user.age} years old")
+
+def get_user():
+    name=input("Name: ").capitalize()
+    while name=="":
+        print("This is a mandatory field")
+        name=input("What is your first name: ").capitalize()
+
+    last_name=input("Last Name: ").capitalize()
+    while last_name=="":
+        print("This is a mandatory field")
+        last_name=input("What is your last name: ").capitalize()
+
+    nationality=input("Nationality: ").capitalize()
+    while nationality=="":
+        print("This is a mandatory field")
+        nationality=input("What is your nationality: ")
+    while True:
+                try:
+                    age=int(input("What is your age: "))
+                    break
+                except ValueError:
+                    print("Use numbers to type your age")
+                    continue
+    user=User(name, last_name, nationality, age)
+    return user
+
+if __name__=="__main__":
+    collecting()
