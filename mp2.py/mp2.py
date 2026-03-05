04f7f7ee4b58d675e7ee349f77b98bd851e58086
Sonia-P5 <sp102@kent.ac.uk>
2026-03-05 09:57:43 +0000

    Turning the menu into a function.

--- mp2.py/mp2.py
+++ mp2.py/mp2.py
@@ -1,15 +1,18 @@
 def main():
-    print("---WELCOME TO THE PORTAL---")
-    print("Clubs available at the Smile Factory Facility")
-    print("Hockey, Tennis, Basketball, and Football")
-    print("MENU")
-    print("1. Create an Account")
-    print("2. Overview of each club")
-    print("3. Select a club(s) (Maximum 2)")
-    print("4. Pay subscription for club")
-    print("5. Exit")
+    def options():
+        print("---WELCOME TO THE PORTAL---")
+        print("Clubs available at the Smile Factory Facility")
+        print("Hockey, Tennis, Basketball, and Football")
+        print("MENU")
+        print("1. Create an Account")
+        print("2. Overview of each club")
+        print("3. Select a club(s) (Maximum 2)")
+        print("4. Pay subscription for club")
+        print("5. Exit")
     
-    a=input("Select option: ")
+        a=input("Select option: ")
+
+    options()
 
 if __name__=="__main__":
     main()
