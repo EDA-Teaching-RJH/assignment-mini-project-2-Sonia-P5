f2cb51d010183c9c901feeff5673d129b500e5b2
Sonia-P5 <sp102@kent.ac.uk>
2026-03-05 20:11:29 +0000

    Creating a menu

--- Smile Fun Factory/body_of_code.py
+++ Smile Fun Factory/body_of_code.py
@@ -3,3 +3,17 @@ def main():
     Hockey=[]
     Tennis=[]
     Basketball=[]
+
+    def options():
+        print("---WELCOME TO THE PORTAL---")
+        print("Clubs available at the Smile Factory Facility")
+        print("Hockey, Tennis, and Basketball")
+        print("Menu")
+        print("1. Create an Account")
+        print("2. Overview of each club")
+        print("3. Select a club(2) (Maximum 2)")
+        print("4. Pay Subscription for club")
+        print("5. Exit")
+
+        a=input("Select option: ")
+    
